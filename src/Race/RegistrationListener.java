1cfdf9aa929b05ee4a1200eef05aabe0df627617
RLG65 <rachalia.inform@gmail.com>
2025-09-27 02:26:38 -0400

    Create RegistrationListener.java

---
+++ src/Race/RegistrationListener.java
@@ -0,0 +1,10 @@
+package Race;
+
+public class RegistrationNotifier implements RegistrationListener {
+    @Override
+    public void onRegistrationComplete(Registration registration) {
+        System.out.println(registration.getRacer().getName()
+                + " has successfully registered with ID "
+                + registration.getRegID());
+    }
+}
