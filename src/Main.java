ffe955c088ff2abc0e07ea1c551789b5d9433e2f
Max <mackenziemax13@gmail.com>
2025-09-26 17:59:22 -0700

    Test and Setup

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import Users.*;
 
 public class Main {
     public static void main(String[] args) {
-        RacerFactory userFactory = new RacerFactory();
+        UserFactory racerFactory = new RacerFactory();
 
-        Racer testRacer = userFactory.createUser("John Doe", "Doe@yahoo.com", "password1234");
+        User testRacer = racerFactory.createUser("John Doe", "Doe@yahoo.com", "password1234");
         System.out.println(testRacer.login("Doe@yahoo.com", "password1234"));
     }
 }
